3a94befd8fbb8deb0322f4863e49371b8dcc6ee5
Kush <156246549+kushvash@users.noreply.github.com>
2025-03-16 15:28:05 -0700

    LeetCode Submission - Valid Palindrome Ii

--- Valid Palindrome Ii/Valid Palindrome Ii.cpp
+++ Valid Palindrome Ii/Valid Palindrome Ii.cpp
@@ -2,28 +2,27 @@ class Solution {
 public:
     bool validPalindrome(string s) {
         int left=0, right=s.length()-1;
-        bool first = true;
 
         while(left<right){
             if(s[left]!=s[right]){
-                if(first){
-                    if(s[left]==s[right-1]){
-                        right--;
-                        first=false;
-                    }else if(s[left+1]==s[right]){
-                        left++;
-                        first=false;
-                    }else{
-                        return false;
-                    }
-                }else{
-                    return false;
-                }
-            }else{
-                left++;
-                right--;
+                
             }
+            left++;
+            right--;
         }
+        return true;
+    }
+
+private:
+    bool isPalindrome(string s, int left, int right){
+        while(left<right){
+            if(s[left]!=s[right]){
+                return false;   
+            }
+            left++;
+            right--;
+        }
+
         return true;
     }
 };
