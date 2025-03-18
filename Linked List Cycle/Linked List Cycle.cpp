01b167ae123aaf2c0ffc29deba7c453565cedb5f
Kush <156246549+kushvash@users.noreply.github.com>
2025-03-17 23:46:02 -0700

    LeetCode Submission - Linked List Cycle

--- Linked List Cycle/Linked List Cycle.cpp
+++ Linked List Cycle/Linked List Cycle.cpp
@@ -11,12 +11,13 @@ public:
     bool hasCycle(ListNode *head) {
         ListNode *slow=head, *fast=head;
 
-        while(fast != nullptr && fast->next != nullptr){
-            if(fast==slow){
-                return true;
-            } 
+        while(fast != nullptr && fast->next != nullptr){ 
             fast=fast->next->next;
             slow=slow->next;
+
+            if(fast==slow){
+                return true;
+            }
         }
 
         return false;
