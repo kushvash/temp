b97dd812040d4838aecd469ee31784c102490919
Kush <156246549+kushvash@users.noreply.github.com>
2025-03-18 00:27:47 -0700

    LeetCode Submission - Happy Number

---
+++ Happy Number/Happy Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        if(n==1){
+            return true;
+        }else if(n>1 && n<10){
+            return false;
+        }
+
+        int k=n, p=0;
+
+        while(k>0){
+            p+=k%10;
+            k/=10;
+        }
+
+        return isHappy(p);
+    }
+};
