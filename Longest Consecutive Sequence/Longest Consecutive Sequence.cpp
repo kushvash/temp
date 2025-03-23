333aa2b089ef7860f14b51dc350cf43376f11f0d
Kush <156246549+kushvash@users.noreply.github.com>
2025-03-22 23:55:06 -0700

    LeetCode Submission - Longest Consecutive Sequence

---
+++ Longest Consecutive Sequence/Longest Consecutive Sequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int curr=nums[0], l=1, maxL=1;
+
+        for(int i=1; i<nums.size(); i++) {
+            if(nums[i]==curr){
+                continue;
+            }
+            if(nums[i]==curr+1) {
+                curr++;
+                l++;
+                maxL=max(l, maxL);
+            }
+            else {
+                curr=nums[i];
+                l=1;
+            }
+        }
+
+        return maxL;
+    }
+};
