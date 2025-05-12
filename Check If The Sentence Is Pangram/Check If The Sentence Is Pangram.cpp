f01567b38afb345bc5df6697db0b02fb0faf5735
Kush <156246549+kushvash@users.noreply.github.com>
2025-05-11 19:25:04 -0700

    LeetCode Submission - Check If The Sentence Is Pangram

---
+++ Check If The Sentence Is Pangram/Check If The Sentence Is Pangram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        for(char c='a'; c<'z'; c++){
+            bool helper=false;
+            for(char cN:sentence){
+                if(cN==c){
+                    helper=true;
+                    break;
+                }
+            }
+
+            if(!helper){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
