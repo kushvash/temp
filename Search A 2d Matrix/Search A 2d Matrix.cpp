19fcd77755a9c2ef1b15d9f52f1bdf6be1791b21
Kush <156246549+kushvash@users.noreply.github.com>
2025-05-13 21:55:43 -0700

    LeetCode Submission - Search A 2d Matrix

---
+++ Search A 2d Matrix/Search A 2d Matrix.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int lo=0, hi=matrix.size()-1, mid;
+
+        while(lo<=hi){
+            mid=lo+((hi-lo)/2);
+
+            if(matrix[mid][0]==target){
+                return true;
+            }else if(matrix[mid][0]>target){
+                hi=mid-1;
+            }else{
+                lo=mid+1;
+            }
+        }
+
+
+        int n=hi;
+
+        if(matrix[n][0]>target){
+            return false;
+        }
+        
+        lo=0;
+        hi=matrix[n].size()-1;
+
+        while(lo<=hi){
+            mid=lo+((hi-lo)/2);
+
+            if(matrix[n][mid]==target){
+                return true;
+            }else if(matrix[n][mid]>target){
+                hi=mid-1;
+            }else{
+                lo=mid+1;
+            }
+        }
+
+        return false;
+    }
+};
