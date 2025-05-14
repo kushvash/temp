6d186c080ef165ff6b02a44659b3d09ccb7f5b4c
Kush <156246549+kushvash@users.noreply.github.com>
2025-05-13 20:44:18 -0700

    LeetCode Submission - Binary Search

--- Binary Search/Binary Search.cpp
+++ Binary Search/Binary Search.cpp
@@ -3,18 +3,19 @@ public:
     int search(vector<int>& nums, int target) {
         int lo=0, hi=nums.size()-1, mid;
 
-        while(lo<=hi) {
-            mid=lo+(hi-lo)/2;
+        while(lo<hi){
+            mid=lo+((hi-lo)/2);
 
-            if(nums[mid]==target) {
+            if(nums[mid]==target){
                 return mid;
-            }else if(nums[mid]>target) {
+            }else if(nums[mid]>target){
                 hi=mid-1;
-            }else {
+            }else{
                 lo=mid+1;
             }
         }
 
         return -1;
+
     }
 };
