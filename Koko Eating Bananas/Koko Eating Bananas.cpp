a6a7d162355837736b0f89c4c76f4503be4b5397
Kush <156246549+kushvash@users.noreply.github.com>
2025-05-14 12:13:39 -0700

    LeetCode Submission - Koko Eating Bananas

---
+++ Koko Eating Bananas/Koko Eating Bananas.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int bananasFinished(vector<int>& piles, int speed){
+        int total=0;
+        for(int i=0; i<piles.size(); i++){
+            total+=piles[i]/speed;
+
+            if(piles[i]%speed!=0){
+                total++;
+            }
+        }
+
+        return total;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int lo=1, mid, hi=0;
+
+        for(int num: piles){
+            hi=max(hi, num);
+        }
+
+        while(lo<=hi){
+            mid=lo+((hi-lo)/2);
+
+            if(bananasFinished(piles, mid)<=h){
+                hi=mid-1;
+            }else{
+                lo=mid+1;
+            }
+        }
+
+        return lo;
+    }
+};
