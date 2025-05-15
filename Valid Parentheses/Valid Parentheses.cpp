c823e24093842adad28f3a73a490732628d09222
Kush <156246549+kushvash@users.noreply.github.com>
2025-05-15 14:27:44 -0700

    LeetCode Submission - Valid Parentheses

---
+++ Valid Parentheses/Valid Parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> helper;
+        unordered_map<char, char> dict={
+            {'}', '{'},
+            {']', '['},
+            {')', '('}
+        };
+
+        for(char c: s){
+            if(c=='{' || c=='[' || c=='('){
+                helper.push(c);
+            }else{
+                if(helper.top()!=dict[c] || helper.empty()){
+                    return false;
+                }
+                helper.pop();
+            }
+        }
+
+        return helper.empty();
+    }
+};
