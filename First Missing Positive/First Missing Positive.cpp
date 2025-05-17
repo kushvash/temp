0ddc9eb9ad45254b283e2eb30bc5cc1d4f36ad18
Kush <156246549+kushvash@users.noreply.github.com>
2025-05-17 12:16:18 -0700

    LeetCode Submission - First Missing Positive

--- First Missing Positive/First Missing Positive.cpp
+++ First Missing Positive/First Missing Positive.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         for(int i=0; i<nums.size(); i++){
-            while(nums[i]!=i){
-                if(nums[i]<0 || nums[i]>=nums.size()){
+            while(nums[i]!=i+1){
+                if(nums[i]<=0 || nums[i]>=nums.size()){
                     break;
                 }
-                swap(nums[i], nums[nums[i]]);
+                swap(nums[i], nums[nums[i]-1]);
             }
         }
 
-        for(int i=1; i<nums.size(); i++){
-            if(nums[i]!=i){
-                return i;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]!=i+1){
+                return i+1;
             }
         }
 
